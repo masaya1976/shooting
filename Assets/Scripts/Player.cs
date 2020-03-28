43e30cab779bdce085a5ed6889ea776ae5b319ef
masaya1976 <masaya1976@gmail.com>
2020-03-28 19:53:22 +0900

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Token
+{
+    public Sprite Spr0; 
+    public Sprite Spr1;
+
+    int _tAnim = 0;
+
+    public float MoveSpeed = 5.0f;
+
+
+    void FixedUpdate()
+    {
+        _tAnim++;
+        if (_tAnim % 48 < 24)
+        {
+            SetSprite(Spr0); //
+        }
+        else
+        {
+            SetSprite(Spr1);
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var w = SpriteWidth / 2;
+        var h = SpriteHeight / 2;
+        SetSize(w, h);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // idou shori
+        Vector2 v = Util.GetInputVector();
+        float speed = MoveSpeed * Time.deltaTime;
+
+        //idousite sotoni denaiyouni suru
+        ClampScreenAndMove(v * speed);
+
+    }
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : Token
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOutside())
+        {
+            // gamenngai ni detanode kesu
+            DestroyObj();
+        }    
+    }
+}
