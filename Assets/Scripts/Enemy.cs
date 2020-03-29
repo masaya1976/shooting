612c722e90a71d6e6c932984fbded00b5a667947
masaya1976 <masaya1976@gmail.com>
2020-03-29 11:41:00 +0900

    add particle

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Token
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //レイヤー名を取得する
+        string name = LayerMask.LayerToName(collision.gameObject.layer);
+
+        if(name == "Shot")
+        {
+            Shot s = collision.GetComponent<Shot>();
+            s.Vanish();
+        }
+    }
+}
